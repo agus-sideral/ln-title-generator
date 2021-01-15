dc7e6896c8d7f83e4b7a3b6c03a10a41464021ef
tini <rabunovels@gmail.com>
2021-01-15 14:40:57 -0300

    :female_detective: added new data. Renamed task name of friendship-task and removed print of @s of followers/unfollowers

--- create_friendship.py
+++ create_friendship.py
@@ -11,11 +11,11 @@ api = twitter.api()
 for follower in tweepy.Cursor(api.followers).items():
     if not follower.following and not follower.protected:
       api.create_friendship(follower.id)
-      print("Now " + follower.screen_name + " is my best friend.")
+      print("Hello new friend! owo")
 
 # Destroy frienships x.x (Unfollow unfollowers)
 for friend in tweepy.Cursor(api.friends_ids).items():
     friendship = api.show_friendship(source_id=os.getenv("ACCOUNT_ID"), target_id=str(friend))
     if not friendship[0].followed_by:
       api.destroy_friendship(friend)
-      print("Friendship ended with " + friendship[1].screen_name + ".")
+      print("Bye-bye friend ;_;")
